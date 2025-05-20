60feff62242cddb5f06889d5c678e7372ecc1f5f
m4theussouz4 <mcapucho1@gmail.com>
2025-05-20 09:52:27 -0300

    add app.config

---
+++ src/app/app.config.ts
@@ -0,0 +1,21 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { environment } from '../environments/environment.prod';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: { preset: Aura }
+    })
+  ]
+};
