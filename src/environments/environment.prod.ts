4b3aa803bfeecfb33480ef27f67a90ce30ba46f7
m4theussouz4 <mcapucho1@gmail.com>
2025-05-20 09:29:06 -0300

    add environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        projectId: "alugmotosapi",
+        appId: "1:718004718057:web:768f8e683ab7eca592e378",
+        storageBucket: "alugmotosapi.firebasestorage.app",
+        apiKey: "AIzaSyAgJ7lYhhqpDgyB9RSDOH1nz6T6VJ8Cjvs",
+        authDomain: "alugmotosapi.firebaseapp.com",
+        messagingSenderId: "718004718057",
+        measurementId: "G-YWBJM8CY3S"
+    }
+};
